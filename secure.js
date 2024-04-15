3f14c6e42f7f71b73e6f041da6a7070abc7da987
ojgraham1 <ojgraham1@gmail.com>
2024-04-15 13:37:15 -0400

    secure

---
+++ secure.js
@@ -0,0 +1,8 @@
+//create string with message
+let string1= "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//assign variables corresponding to each of the three codes in the combination
+const one= 5+5;
+const two= 50-10;
+const three= 3*13;
+//create a dialog box displaying the vault codes
+let code= prompt(string1+one+"-"+two+"-"+three);
